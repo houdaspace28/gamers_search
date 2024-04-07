c46e023c6623701db180ee9c6f85e780dad9b18d
Anonymoush-28 <houdabssd@gmail.com>
2024-04-07 23:17:21 +0000

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-
+import {Grid, GridItem, Show} from '@chakra-ui/react'
 function App() {
   return (
     <>
-      
+      <Grid templateAreas={{
+        base:`"nav" "main"`,
+        lg: `"nav nav" "aside main"`,//screens wider tha 1024px
+      }}>
+        <GridItem area='nav' >
+
+        </GridItem>
+        <Show above='lg'>
+        <GridItem area='aside'>
+
+        </GridItem>
+        </Show>
+        <GridItem area='main'>
+
+        </GridItem>
+
+      </Grid>
     </>
   )
 }
