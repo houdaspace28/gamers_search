35b191918edd785bd963c83a71a9bb4b7be1042c
Anonymoush-28 <houdabssd@gmail.com>
2024-04-07 23:25:25 +0000

    NavBar initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {Grid, GridItem, Show} from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 function App() {
   return (
     <>
@@ -7,7 +8,7 @@ function App() {
         lg: `"nav nav" "aside main"`,//screens wider tha 1024px
       }}>
         <GridItem area='nav' >
-
+           <NavBar/>
         </GridItem>
         <Show above='lg'>
         <GridItem area='aside'>
